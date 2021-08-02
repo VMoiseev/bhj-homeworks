bbcebae2d6d514404358a42822a4ac26adb8d2e5
VMoiseev <e175@inbox.ru>
2021-08-02 14:40:19 +0500

    Задание _слайдер изображений_ готово

---
+++ element-search/slider/task.js
@@ -0,0 +1,49 @@
+const sliders = Array.from(document.getElementsByClassName("slider__item"));
+const dots = Array.from(document.getElementsByClassName("slider__dot"));
+
+const arrowPrev = document.querySelector(".slider__arrow_prev");
+const arrowNext = document.querySelector(".slider__arrow_next");
+
+let activeSlider = 0;
+const allSliders = sliders.length;
+
+function removableSlide() {
+  sliders[activeSlider].className = "slider__item";
+  dots[activeSlider].className = "slider__dot";
+}
+
+function installableSlide() {
+  sliders[activeSlider].className = "slider__item slider__item_active";
+  dots[activeSlider].className = "slider__dot slider__dot_active";
+}
+
+arrowPrev.onclick = function() {
+  removableSlide();
+  activeSlider--;
+
+  if (activeSlider < 0) { 
+    activeSlider = allSliders - 1;
+  }
+  installableSlide();
+}
+
+arrowNext.onclick = function() {
+  removableSlide();
+  activeSlider++;
+
+  if (activeSlider >= allSliders) {
+    activeSlider = 0;
+  }
+  installableSlide();
+}
+
+dots.forEach(function(dot) {
+  if (dot.classList.contains("slider__dot slider__dot_active")) {
+    return;
+  } else {
+    removableSlide();
+    installableSlide();
+  }
+});
+
+//При кликах по точкам никак не переключается, что-то упустил...и напутал(
