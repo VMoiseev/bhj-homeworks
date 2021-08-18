bc2c82172599a6ce12d265897da5f3f964991a48
VMoiseev <e175@inbox.ru>
2021-08-18 09:28:17 +0500

    ДЗ_Простой список дел_готово

---
+++ document-structure/todo/task.js
@@ -0,0 +1,26 @@
+const taskInput = document.getElementById("task__input");
+const taskList = document.getElementById("tasks__list");
+const tasksForm = document.getElementById("tasks__form");
+
+tasksForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (taskInput.value) {
+    taskList.innerHTML += `
+    <div class="task">
+      <div class="task__title">
+        ${taskInput.value}
+      </div>
+      <a href="#" class="task__remove">&times;</a>
+    </div>
+    `
+    taskInput.value = "";
+  }
+});
+
+taskList.addEventListener("click", e => {
+
+  if (e.target.classList.contains("task__remove")) {
+    e.target.parentElement.remove();
+  }
+});
