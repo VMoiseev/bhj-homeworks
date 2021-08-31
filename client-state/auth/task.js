43d1fa8c9ad7b4cf14c06026ec5020bb294ad579
VMoiseev <e175@inbox.ru>
2021-08-31 23:33:52 +0500

    Сделал сохранение id пользователя в лок.хранилище

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,21 +1,9 @@
-const signinForm = document.getElementById("signin__form");
 const signin = document.getElementById("signin");
+const signinForm = document.getElementById("signin__form");
 const signinBtn = document.getElementById("signin__btn");
 const welcome = document.getElementById("welcome");
 const userId = document.getElementById("user_id");
 let user = localStorage.user_id;
-
-function welcomeUser() {
-  signin.classList.remove("signin_active");
-  welcome.classList.add("welcome_active");
-  userId.textContent = `${user}`;
-}
-
-if(user){
-  welcomeUser();
-} else {
-  signin.classList.add('signin_active');
-}
 
 signinBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -26,15 +14,27 @@ signinBtn.addEventListener("click", (e) => {
   xhr.open("POST", URL);
 
   xhr.addEventListener("load", () => {
-    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-      if (!xhr.response.success) {
-        alert("Неверный логин/пароль");
-      } else {
-        user = xhr.response.user_id;
-        welcomeUser();
-      }
-      signinForm.reset();
+    if (!xhr.response.success) {
+      alert("Неверный логин/пароль");
+    } else {
+      welcomeUser();
+      userId.innerText = id;
+      localStorage.setItem("user", id);
     }
+    signinForm.reset();
   });
   xhr.send(formData);
 });
+
+function welcomeUser() {
+  signin.classList.remove("signin_active");
+  welcome.classList.add("welcome_active");
+}
+
+window.addEventListener("load", () => {
+  const storageId = localStorage.getItem("user");
+  if (storageId) {
+    welcomeUser ();
+    userId.innerText = storageId;
+  }
+})
